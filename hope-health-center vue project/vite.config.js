f3f70b1d474067fedf78ba3b220d1493aeaf9ac6
kevinboyz <visionboyz66@gmail.com>
2026-05-05 18:36:45 +0200

    Update base path in Vite configuration
    Set the base path for the project to '/maths/'.

--- hope-health-center vue project/vite.config.js
+++ hope-health-center vue project/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/maths/', // Iyi niyo ntambwe y'ingenzi yerekana aho umushinga uherereye
   plugins: [vue()],
   resolve: {
     alias: {
